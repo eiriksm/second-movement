03bf7108bd88752105067a3c5f4daed08df7e64c
eiriksm <eiriksm@users.noreply.github.com>
2025-01-11 19:15:28 +0000

    deploy: dce2187ec170d8d98db02cde2d4394ea83df7b89

---
+++ search/enums_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['watch_5findicator_5ft_0',['watch_indicator_t',['../group__slcd.html#ga06c1b2d571f8ae94b44ac56f6ff4ec00',1,'watch_slcd.h']]],
+  ['watch_5fposition_5ft_1',['watch_position_t',['../group__slcd.html#gaa60b55d055f178d17e511e8fe3075494',1,'watch_slcd.h']]]
+];
