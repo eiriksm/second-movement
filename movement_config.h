40475d8aa62e2420e29eb15274bee85b5025991b
Eirik S. Morland <eirik@morland.no>
2025-01-13 10:52:35 +0100

    Need to set the time I guess

--- movement_config.h
+++ movement_config.h
@@ -33,6 +33,7 @@ const watch_face_t watch_faces[] = {
     bac_face,
     fast_stopwatch_face,
     voltage_face,
+    set_time_face,
 };
 
 #define MOVEMENT_NUM_FACES (sizeof(watch_faces) / sizeof(watch_face_t))
@@ -43,7 +44,7 @@ const watch_face_t watch_faces[] = {
  * Some folks also like to use this to hide the preferences and time set faces from the normal rotation.
  * If you don't want any faces to be excluded, set this to 0 and a long Mode press will have no effect.
  */
-#define MOVEMENT_SECONDARY_FACE_INDEX 0
+#define MOVEMENT_SECONDARY_FACE_INDEX (MOVEMENT_NUM_FACES - 1)
 
 /* Custom hourly chime tune. Check movement_custom_signal_tunes.h for options. */
 #define SIGNAL_TUNE_DEFAULT
