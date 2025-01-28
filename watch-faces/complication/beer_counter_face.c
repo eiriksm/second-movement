853766a33b0c51818fc9ef39cf0f3904f2e73e5a
Eirik S. Morland <eirik@morland.no>
2025-01-28 08:58:13 +0100

    Update with edit screen

--- watch-faces/complication/beer_counter_face.c
+++ watch-faces/complication/beer_counter_face.c
@@ -17,20 +17,18 @@ void beer_counter_face_setup(uint8_t watch_face_index, void ** context_ptr) {
         state->beer_count = 0;
         state->weight = 95;
         state->sex = 0;
+        state->edit_on = false;
     }
 }
 
-static void print_beer_count(beer_counter_state_t *state) {
+static void print_unit_count(beer_counter_state_t *state) {
     char buf[4];
     // We actually do not want to pad with 0 but with space.
     sprintf(buf, "%2d", state->beer_count);
-    printf("Beer count: %s\n", buf);
-    printf("beer count from state %d\n", state->beer_count);
     watch_display_text_with_fallback(WATCH_POSITION_TOP_LEFT, "BC", "BC");
     watch_display_text(WATCH_POSITION_TOP_RIGHT, buf);
     // Let's also get the BAC.
     float bac = calculate_bac(state);
-    printf("BAC: %f\n", bac);
     char result[3][8];
     parse_bac_into_result(bac, result);
     char hour_buf[11];
@@ -63,6 +61,7 @@ void parse_bac_into_result(float val, char result[3][8]) {
 
 static float calculate_bac(beer_counter_state_t *state) {
     float result;
+    float alcohol_g = 0.0;
     watch_date_time_t now = movement_get_local_date_time();
     uint32_t current_time_unix = watch_utility_date_time_to_unix_time(now, movement_get_current_timezone_offset());
     uint32_t time_elapsed_seconds = current_time_unix - state->start_time;
@@ -72,17 +71,16 @@ static float calculate_bac(beer_counter_state_t *state) {
     if (state->beer_count == 0) {
         return 0;
     }
-    printf("time_elapsed_seconds: %d\n", time_elapsed_seconds);
-
-    float alcohol_g = (state->beer_count * 500 * 4.7 * ALCOHOL_DENSITY)/100; //because we put in drink_vol as integers we have to divide by 100
-    printf("alcohol_g: %f\n", alcohol_g);
+    for (int i = 0; i < state->beer_count; i++) {
+        float unit_g = (state->units[i].volume * (state->units[i].percentage / 10) * ALCOHOL_DENSITY) / 100;
+        alcohol_g += unit_g;
+    }
     if (state->sex == 1) {
         result = bac_for_women_from_weight_and_alcohol_grams(state->weight, alcohol_g, state->start_time, current_time_unix);
     }
     else {
         result = bac_for_men_from_weight_and_alcohol_grams(state->weight, alcohol_g, state->start_time, current_time_unix);
     }
-    printf("result: %f\n", result);
     return result;
 }
 
@@ -95,9 +93,71 @@ void beer_counter_face_activate(void *context) {
     float bac = calculate_bac(state);
     if (bac == 0) {
         state->beer_count = 0;
-        state->old_bac = 0;
     }
-    print_beer_count(state);
+    print_unit_count(state);
+}
+
+void draw_screen(beer_counter_state_t *state) {
+    if (state->screen_delta == 0) {
+        print_unit_count(state);
+    }
+    if (state->screen_delta == 1) {
+        print_edit_screen(state);
+    }
+}
+
+void print_edit_screen(beer_counter_state_t *state) {
+    char buf[10];
+    int delta = state->beer_count - state->edit_offset;
+    sprintf(buf, "%2d", delta);
+    watch_display_text_with_fallback(WATCH_POSITION_TOP_LEFT, "ED", "EDT");
+    watch_display_text(WATCH_POSITION_TOP_RIGHT, buf);
+    // There is one value to display, and another to talk about to the computer.
+    int computer_delta = delta - 1;
+    int volume = state->units[computer_delta].volume;
+    int percentage = state->units[computer_delta].percentage;
+    // Depending on edit mode or not, we want to display different things.
+    if (state->edit_on) {
+        // Check if we are editing volume or percentage.
+        if (state->is_alc_cont_screen) {
+            // Blank it out first.
+            int tens = percentage * 10;
+            watch_display_text(WATCH_POSITION_HOURS, "  ");
+            watch_display_text(WATCH_POSITION_MINUTES, "  ");
+            watch_display_text(WATCH_POSITION_SECONDS, "  ");
+            sprintf(buf, "%d", tens % 100);
+            watch_display_text(WATCH_POSITION_SECONDS, buf);
+            sprintf(buf, "%2d", (tens - (tens % 100) ) / 100);
+            watch_display_text(WATCH_POSITION_MINUTES, buf);
+        }
+        else {
+            sprintf(buf, "%d", volume);
+            watch_display_text(WATCH_POSITION_BOTTOM, buf);
+        }
+        // Blink it a bit.
+        watch_date_time_t now = movement_get_local_date_time();
+        uint32_t current_time_unix = watch_utility_date_time_to_unix_time(now, movement_get_current_timezone_offset());
+        if (current_time_unix % 2 == 0) {
+            watch_display_text(WATCH_POSITION_HOURS, "  ");
+            watch_display_text(WATCH_POSITION_MINUTES, "  ");
+            watch_display_text(WATCH_POSITION_SECONDS, "  ");
+        }
+    }
+    else {
+        sprintf(buf, "%d", volume);
+        // Split the volume in two parts. First part should be the first number.
+        char first_part[3];
+        sprintf(first_part, " %d", volume / 100);
+        watch_display_text(WATCH_POSITION_HOURS, first_part);
+
+        char second_part[3];
+        second_part[0] = buf[1];
+        second_part[1] = buf[2];
+        second_part[2] = '\0';
+        watch_display_text(WATCH_POSITION_MINUTES, second_part);
+        sprintf(buf, "%d", percentage);
+        watch_display_text(WATCH_POSITION_SECONDS, buf);
+    }
 }
 
 bool beer_counter_face_loop(movement_event_t event, void *context) {
@@ -105,34 +165,113 @@ bool beer_counter_face_loop(movement_event_t event, void *context) {
 
     switch (event.event_type) {
         case EVENT_TICK:
-            print_beer_count(state);
+            draw_screen(state);
             break;
 
         case EVENT_ACTIVATE:
-            state->is_weight_screen = false;
-            state->is_sex_screen = false;
-            state->is_bac_screen = false;
-            state->is_sober_screen = false;
-            state->is_drink_vol_screen = false;
-            state->is_alc_cont_screen =false;
-            print_beer_count(state);
+            state->screen_delta = 0;
+            state->edit_on = false;
+            state->edit_offset = 0;
+            print_unit_count(state);
             break;
 
         case EVENT_ALARM_BUTTON_UP:
+            // If we are on the edit screen, we want to change the value.
+            if (state->screen_delta == 1) {
+                if (state->edit_on) {
+                    int delta = state->beer_count - state->edit_offset;
+                    int computer_delta = delta - 1;
+                    if (state->is_alc_cont_screen) {
+                        state->units[computer_delta].percentage += 5;
+                    }
+                    else {
+                        // Cycle through the available volumes.
+                        if (state->units[computer_delta].volume == 300) {
+                            state->units[computer_delta].volume = 330;
+                        }
+                        else if (state->units[computer_delta].volume == 330) {
+                            state->units[computer_delta].volume = 400;
+                        }
+                        else if (state->units[computer_delta].volume == 400) {
+                            state->units[computer_delta].volume = 500;
+                        }
+                        else if (state->units[computer_delta].volume == 500) {
+                            state->units[computer_delta].volume = 600;
+                        }
+                        else if (state->units[computer_delta].volume == 600) {
+                            state->units[computer_delta].volume = 700;
+                        }
+                        else if (state->units[computer_delta].volume == 700) {
+                            state->units[computer_delta].volume = 750;
+                        }
+                        else if (state->units[computer_delta].volume == 750) {
+                            state->units[computer_delta].volume = 300;
+                        }
+                    }
+                }
+                else {
+                    // Cycle the units.
+                    state->edit_offset++;
+                    if (state->edit_offset >= state->beer_count) {
+                        state->edit_offset = 0;
+                    }
+                }
+                print_edit_screen(state);
+                break;
+            }
+            // Append one more item to the units array.
             state->beer_count++;
+            state->units[state->beer_count - 1].volume = 500;
+            state->units[state->beer_count - 1].percentage = 45;
             // If the start time is not set, set it to the current timestamp.
             if (state->start_time == 0) {
                 watch_date_time_t date_time = movement_get_local_date_time();
                 state->start_time = watch_utility_date_time_to_unix_time(date_time, movement_get_current_timezone_offset());
             }
-            print_beer_count(state);
+            print_unit_count(state);
+            break;
+
+        case EVENT_LIGHT_BUTTON_UP:
+            // If the count is 0, then do nothing.
+            if (state->beer_count == 0) {
+                break;
+            }
+            // Cycle the modes. Unless in edit mode.
+            if (state->screen_delta == 1 && state->edit_on) {
+                if (!state->is_alc_cont_screen) {
+                    state->is_alc_cont_screen = true;
+                }
+                else {
+                    state->is_alc_cont_screen = false;
+                    state->edit_offset++;
+                }
+                if (state->edit_offset >= state->beer_count) {
+                    state->edit_offset = 0;
+                }
+                print_edit_screen(state);
+                break;
+            }
+            state->screen_delta++;
+            if (state->screen_delta > 1) {
+                state->screen_delta = 0;
+            }
+            draw_screen(state);
             break;
 
         case EVENT_LIGHT_LONG_PRESS:
-            // Reset.
+            // Reset. Unless in edit mode.
+            if (state->screen_delta == 1) {
+                state->edit_on = !state->edit_on;
+                if (state->edit_on) {
+                    state->edit_offset = 0;
+                    state->is_alc_cont_screen = false;
+                }
+                print_edit_screen(state);
+                break;
+            }
             state->beer_count = 0;
             state->start_time = 0;
-            print_beer_count(state);
+            print_unit_count(state);
             break;
 
         case EVENT_TIMEOUT:
--- watch-faces/complication/beer_counter_face.h
+++ watch-faces/complication/beer_counter_face.h
@@ -5,19 +5,21 @@
 #include "movement.h"
 
 typedef struct {
+    struct {
+        int volume;
+        // Alcohol percentage. 4.7 would be stored as 47.
+        int percentage;
+    } units[20];
     uint8_t beer_count;
     uint32_t start_time; // Store start_time time of beer consumption
     uint32_t weight;
     uint8_t sex; // 0 for male, 1 for female
     uint32_t drink_vol;
     uint32_t alc_cont;
-    bool is_weight_screen;
-    bool is_sex_screen;
-    bool is_bac_screen;
-    bool is_sober_screen;
-    bool is_drink_vol_screen; //volume of drink
+    uint8_t screen_delta;
+    uint8_t edit_offset;
+    bool edit_on;
     bool is_alc_cont_screen; //alcohol content of drink
-    float old_bac;
 } beer_counter_state_t;
 
 void beer_counter_face_setup(uint8_t watch_face_index, void ** context_ptr);
@@ -25,9 +27,10 @@ void beer_counter_face_activate(void *context);
 bool beer_counter_face_loop(movement_event_t event, void *context);
 void beer_counter_face_resign(void *context);
 void parse_bac_into_result(float val, char result[3][8]);
+void print_edit_screen(beer_counter_state_t *state);
 
 static float calculate_bac(beer_counter_state_t *state);
-static void print_beer_count(beer_counter_state_t *state);
+static void print_unit_count(beer_counter_state_t *state);
 
 
 static uint32_t calculate_time_to_sober(float current_bac);
