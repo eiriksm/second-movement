2ec1415a458f5120d9283997967fffb60765a00d
Eirik Stanghelle Morland <eirik@morland.no>
2025-01-31 08:03:44 +0100

    Make sure start time is reset when time runs out (#3)

--- watch-faces/test/mock/watch.c
+++ watch-faces/test/mock/watch.c
@@ -1,4 +1,6 @@
 #include "watch.h"
+#include <time.h>
+#include <stdio.h>
 
 void watch_set_led_off(void) {}
 void watch_display_text(watch_position_t location, const char *string) {}
@@ -6,7 +8,17 @@ void watch_display_text(watch_position_t location, const char *string) {}
 void watch_display_text_with_fallback(watch_position_t location, const char *string, const char *fallback) {}
 
 uint32_t watch_utility_date_time_to_unix_time(watch_date_time_t date_time, int32_t utc_offset) {
-    (void) date_time;
-    (void) utc_offset;
-    return 0;
+    // Convert from watch_date_time_t to tm.
+    struct tm time_info;
+    time_info.tm_sec = date_time.unit.second;
+    time_info.tm_min = date_time.unit.minute;
+    time_info.tm_hour = date_time.unit.hour;
+    time_info.tm_mday = date_time.unit.day;
+    time_info.tm_mon = date_time.unit.month - 1;
+    time_info.tm_year = (date_time.unit.year + 2020) - 1900;
+    // This one should be in the UTC timezone.
+    time_info.tm_isdst = 0;
+    // Now convert to unix time.
+    time_t unix_time = mktime(&time_info);
+    return unix_time - (utc_offset * 3600);
 }
