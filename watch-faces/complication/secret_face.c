d01b4a20bd51ee259d1722cd872ac410380ff4d4
Eirik Stanghelle Morland <eirik@morland.no>
2025-02-04 21:02:41 +0100

    Update secret_face.c

--- watch-faces/complication/secret_face.c
+++ watch-faces/complication/secret_face.c
@@ -35,8 +35,7 @@ secret_state_t *my_secret;
 static void draw(secret_state_t *state, uint8_t subsecond) {
     (void) subsecond;
 
-    watch_display_text_with_fallback(WATCH_POSITION_BOTTOM, "SECRET", "    <3");
-    printf("ricks %d\n", state->clicks);
+    watch_display_text_with_fallback(WATCH_POSITION_BOTTOM, "SECRET", "     G");
     if (state->clicks == 3) {
       watch_buzzer_play_sequence(round_win_melody, NULL);
     }
