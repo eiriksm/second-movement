c1eb7de888c1f6aa34c66f83d7e9ac32473d0ee4
eiriksm <eiriksm@users.noreply.github.com>
2025-03-09 07:01:52 +0000

    deploy: a6313b28c082073dfffcb80ce522ce2b2a4c7b48

--- search/enumvalues_1.js
+++ search/enumvalues_1.js
@@ -8,12 +8,15 @@ var searchData=
   ['watch_5findicator_5fpm_5',['WATCH_INDICATOR_PM',['../group__slcd.html#gga06c1b2d571f8ae94b44ac56f6ff4ec00a0d023567f536ace6ff82e5120729090f',1,'watch_slcd.h']]],
   ['watch_5findicator_5fsignal_6',['WATCH_INDICATOR_SIGNAL',['../group__slcd.html#gga06c1b2d571f8ae94b44ac56f6ff4ec00a827a013c00782a143f13a4f88ce50292',1,'watch_slcd.h']]],
   ['watch_5findicator_5fsleep_7',['WATCH_INDICATOR_SLEEP',['../group__slcd.html#gga06c1b2d571f8ae94b44ac56f6ff4ec00ac957d9f75277777347d253f38ddd01ea',1,'watch_slcd.h']]],
-  ['watch_5fposition_5fbottom_8',['WATCH_POSITION_BOTTOM',['../group__slcd.html#ggaa60b55d055f178d17e511e8fe3075494ae9a139a225a138bfad17281524c4f875',1,'watch_slcd.h']]],
-  ['watch_5fposition_5ffull_9',['WATCH_POSITION_FULL',['../group__slcd.html#ggaa60b55d055f178d17e511e8fe3075494af0ef20906a91ccc659e0447154309ec0',1,'watch_slcd.h']]],
-  ['watch_5fposition_5fhours_10',['WATCH_POSITION_HOURS',['../group__slcd.html#ggaa60b55d055f178d17e511e8fe3075494ac6b57b7168134d3fecbd8953ed94ecab',1,'watch_slcd.h']]],
-  ['watch_5fposition_5fminutes_11',['WATCH_POSITION_MINUTES',['../group__slcd.html#ggaa60b55d055f178d17e511e8fe3075494ad9fa8b078543eb49e8a63d9c11205d62',1,'watch_slcd.h']]],
-  ['watch_5fposition_5fseconds_12',['WATCH_POSITION_SECONDS',['../group__slcd.html#ggaa60b55d055f178d17e511e8fe3075494a36f0af2a5e3ef09ee33376683df2c516',1,'watch_slcd.h']]],
-  ['watch_5fposition_5ftop_13',['WATCH_POSITION_TOP',['../group__slcd.html#ggaa60b55d055f178d17e511e8fe3075494af4e3d3c11e929a0401a665c13d617b60',1,'watch_slcd.h']]],
-  ['watch_5fposition_5ftop_5fleft_14',['WATCH_POSITION_TOP_LEFT',['../group__slcd.html#ggaa60b55d055f178d17e511e8fe3075494a3dc18efc3ba32d3313876d678ec0461d',1,'watch_slcd.h']]],
-  ['watch_5fposition_5ftop_5fright_15',['WATCH_POSITION_TOP_RIGHT',['../group__slcd.html#ggaa60b55d055f178d17e511e8fe3075494a8e7dffaac79da1cbb5a1e882f55fc0be',1,'watch_slcd.h']]]
+  ['watch_5flcd_5ftype_5fclassic_8',['WATCH_LCD_TYPE_CLASSIC',['../group__slcd.html#gga0b0f73538886bd4a604f284df7c8ee35ae96a496ac1c9a333d6dfd649c5cd6b69',1,'watch_slcd.h']]],
+  ['watch_5flcd_5ftype_5fcustom_9',['WATCH_LCD_TYPE_CUSTOM',['../group__slcd.html#gga0b0f73538886bd4a604f284df7c8ee35ade10172ef888a971d4055a7908afa827',1,'watch_slcd.h']]],
+  ['watch_5flcd_5ftype_5funknown_10',['WATCH_LCD_TYPE_UNKNOWN',['../group__slcd.html#gga0b0f73538886bd4a604f284df7c8ee35af0f783770a1401844caa5331f6833803',1,'watch_slcd.h']]],
+  ['watch_5fposition_5fbottom_11',['WATCH_POSITION_BOTTOM',['../group__slcd.html#ggaa60b55d055f178d17e511e8fe3075494ae9a139a225a138bfad17281524c4f875',1,'watch_slcd.h']]],
+  ['watch_5fposition_5ffull_12',['WATCH_POSITION_FULL',['../group__slcd.html#ggaa60b55d055f178d17e511e8fe3075494af0ef20906a91ccc659e0447154309ec0',1,'watch_slcd.h']]],
+  ['watch_5fposition_5fhours_13',['WATCH_POSITION_HOURS',['../group__slcd.html#ggaa60b55d055f178d17e511e8fe3075494ac6b57b7168134d3fecbd8953ed94ecab',1,'watch_slcd.h']]],
+  ['watch_5fposition_5fminutes_14',['WATCH_POSITION_MINUTES',['../group__slcd.html#ggaa60b55d055f178d17e511e8fe3075494ad9fa8b078543eb49e8a63d9c11205d62',1,'watch_slcd.h']]],
+  ['watch_5fposition_5fseconds_15',['WATCH_POSITION_SECONDS',['../group__slcd.html#ggaa60b55d055f178d17e511e8fe3075494a36f0af2a5e3ef09ee33376683df2c516',1,'watch_slcd.h']]],
+  ['watch_5fposition_5ftop_16',['WATCH_POSITION_TOP',['../group__slcd.html#ggaa60b55d055f178d17e511e8fe3075494af4e3d3c11e929a0401a665c13d617b60',1,'watch_slcd.h']]],
+  ['watch_5fposition_5ftop_5fleft_17',['WATCH_POSITION_TOP_LEFT',['../group__slcd.html#ggaa60b55d055f178d17e511e8fe3075494a3dc18efc3ba32d3313876d678ec0461d',1,'watch_slcd.h']]],
+  ['watch_5fposition_5ftop_5fright_18',['WATCH_POSITION_TOP_RIGHT',['../group__slcd.html#ggaa60b55d055f178d17e511e8fe3075494a8e7dffaac79da1cbb5a1e882f55fc0be',1,'watch_slcd.h']]]
 ];
