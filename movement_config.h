9c2a155aad73baca71936725877c30ccffffc6b9
Eirik Stanghelle Morland <eirik@morland.no>
2025-08-16 07:51:54 +0200

    Update movement_config.h

--- movement_config.h
+++ movement_config.h
@@ -34,9 +34,10 @@ const watch_face_t watch_faces[] = {
     fast_stopwatch_face,
     countdown_face,
     voltage_face,
-    set_time_face,
+    finetune_face,
+    nanosec_face,
     settings_face,
-    activity_logging_face
+    set_time_face,
 };
 
 #define MOVEMENT_NUM_FACES (sizeof(watch_faces) / sizeof(watch_face_t))
@@ -47,7 +48,7 @@ const watch_face_t watch_faces[] = {
  * Some folks also like to use this to hide the preferences and time set faces from the normal rotation.
  * If you don't want any faces to be excluded, set this to 0 and a long Mode press will have no effect.
  */
-#define MOVEMENT_SECONDARY_FACE_INDEX (MOVEMENT_NUM_FACES - 3)
+#define MOVEMENT_SECONDARY_FACE_INDEX (MOVEMENT_NUM_FACES - 5)
 
 /* Custom hourly chime tune. Check movement_custom_signal_tunes.h for options. */
 #define SIGNAL_TUNE_DEFAULT
