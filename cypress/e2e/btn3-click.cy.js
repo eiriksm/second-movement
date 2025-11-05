cd40188b1fa8a1e1823c76b739d189f0d66207b7
Eirik Stanghelle Morland <eirik@morland.no>
2025-11-05 07:24:34 +0100

    Update btn3-click.cy.js

--- cypress/e2e/btn3-click.cy.js
+++ cypress/e2e/btn3-click.cy.js
@@ -1,15 +1,9 @@
 describe('Button #btn3 Click Test', () => {
   it('should click button #btn3', () => {
-    // Visit the watch simulator HTML file
     cy.visit('build-sim/firmware.html')
-
-    // Find and click button #btn3
     cy.wait(5000)
     cy.get('#btn3').click()
     cy.wait(15000)
-
-    // Optional: Add assertions to verify the click had the expected effect
-    // For example, you might check if a certain element appears or changes
-    // cy.get('#some-element').should('be.visible')
+    cy.get('.not-exist')
   })
 })
